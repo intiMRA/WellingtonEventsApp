3372bac9f2a0dfc0c92a3201a7f76c672d20b801
inti <inti.albuquerque@trademe.co.nz>
2025-07-18 20:05:35 +1200

    more info

--- WellingtonEvents/Details/EventDetailsView.swift
+++ WellingtonEvents/Details/EventDetailsView.swift
@@ -18,27 +18,67 @@ struct EventDetailsView: View {
     }
     
     var body: some View {
-        VStack {
-            if let image = viewModel.image, let location = viewModel.event.location {
-                Button {
-                    openDirectionsInAppleMaps(coordinate: .init(latitude: location.lat, longitude: location.long), arress: viewModel.event.venue)
-                }
-                label: {
-                    Image(uiImage: image)
-                        .resizable()
-                        .frame(width: EventDetailsViewModel.snapshorSize.width, height: EventDetailsViewModel.snapshorSize.height)
-                        .roundedShadow()
-                }
-            }
-            else {
+        ScrollView {
+            imageView(url: viewModel.event.url)
+            Text(viewModel.event.name)
+                .font(.headline)
+                .padding()
+            Text(viewModel.event.description)
+                .padding()
+            mapImage
+
+        }
+        .task {
+            viewModel.generateSnapshot()
+        }
+    }
+    
+    @ViewBuilder
+    func imageView(url: String) -> some View {
+        AsyncImage(url: URL(string: url)) { phase in
+            switch phase {
+            case .empty:
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(.gray)
+            case .success(let image):
+                image
+                    .resizable()
+            case .failure(let error):
+                Image(.noImageAtTime)
+                    .resizable()
+                    .foregroundStyle(.textSecondary)
+                    .onAppear {
+                        print(error)
+                    }
+            @unknown default:
                 Rectangle()
-                    .fill(Color.gray)
+                    .fill(.gray)
+            }
+        }
+        .frame(height: 155)
+        .frame(maxWidth: .infinity)
+        .scaledToFill()
+        .roundedShadow()
+    }
+    
+    @ViewBuilder
+    var mapImage: some View {
+        if let image = viewModel.image, let location = viewModel.event.location {
+            Button {
+                openDirectionsInAppleMaps(coordinate: .init(latitude: location.lat, longitude: location.long), arress: viewModel.event.venue)
+            }
+            label: {
+                Image(uiImage: image)
+                    .resizable()
                     .frame(width: EventDetailsViewModel.snapshorSize.width, height: EventDetailsViewModel.snapshorSize.height)
                     .roundedShadow()
             }
         }
-        .task {
-            viewModel.generateSnapshot()
+        else {
+            Rectangle()
+                .fill(Color.gray)
+                .frame(width: EventDetailsViewModel.snapshorSize.width, height: EventDetailsViewModel.snapshorSize.height)
+                .roundedShadow()
         }
     }
     
